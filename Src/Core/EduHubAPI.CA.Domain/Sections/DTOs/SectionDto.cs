986f6cb5761e846dd0166b28929139058d3fde08
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-10-08 23:22:28 +0700

    added: Section Domain

---
+++ Src/Core/EduHubAPI.CA.Domain/Sections/DTOs/SectionDto.cs
@@ -0,0 +1,30 @@
+﻿using EduHubAPI.CA.Domain.Lessons.DTOs;
+using EduHubAPI.CA.Domain.Products.Entities;
+using EduHubAPI.CA.Domain.Sections.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Xml.Linq;
+
+namespace EduHubAPI.CA.Domain.Sections.DTOs
+{
+    public class SectionDto
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public int Order { get; set; }
+        public string? Description { get; set; }
+
+        public long CourseId { get; set; }
+        public ICollection<LessonDto> Lessons { get; set; }
+
+        public SectionDto() { }
+        public SectionDto(Section section)
+        {
+            Id = section.Id;
+            Title = section.Title;
+
+        }
+    }
+}
---
+++ Src/Core/EduHubAPI.CA.Domain/Sections/Entities/Section.cs
@@ -0,0 +1,26 @@
+﻿using EduHubAPI.CA.Domain.Common;
+using EduHubAPI.CA.Domain.Courses.Entities;
+using EduHubAPI.CA.Domain.Lessons.Entities;
+using System.Collections.Generic;
+
+namespace EduHubAPI.CA.Domain.Sections.Entities
+{
+    public class Section : AuditableBaseEntity
+    {
+        public string Title { get; set; }
+        public int Order { get; set; }
+
+        public string? Description { get; set; }
+
+        public long? CourseId { get; set; }
+        public Course Course { get; set; }
+
+        public ICollection<Lesson> Lessons { get; set; }
+
+
+        public void Update(string title)
+        {
+            Title = title;
+        }
+    }
+}
