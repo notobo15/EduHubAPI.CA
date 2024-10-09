7a4b066d5f4720950cd67e16b83021acd09c3369
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-10-09 23:21:30 +0700

    added: Category domain

---
+++ Src/Core/EduHubAPI.CA.Domain/Categories/DTOs/CategoryDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduHubAPI.CA.Domain.Categories.DTOs
+{
+    public class CategoryDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+        public long? ParentId { get; set; }
+        public string? ParentName { get; set; }
+    }
+}
---
+++ Src/Core/EduHubAPI.CA.Domain/Categories/Entities/Category.cs
@@ -0,0 +1,28 @@
+﻿using EduHubAPI.CA.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduHubAPI.CA.Domain.Courses.Entities
+{
+    public class Category : AuditableBaseEntity
+    {
+        public string Name { get; set; }
+        
+        public string? Description { get; set; }
+
+        public long? ParentId { get; set; }
+        public Category Parent { get; set; }
+
+        public ICollection<Category> Children { get; set; }
+
+        public Category(string name)
+        {
+            Name = name;
+        }
+        public void Update(string name)
+        {
+            Name = name;
+        }
+    }
+}
