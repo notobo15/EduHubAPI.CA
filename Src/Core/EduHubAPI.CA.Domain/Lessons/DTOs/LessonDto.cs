3c02aa662be45d24020293ae7bc2ebe1e9a89d01
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-10-09 23:22:08 +0700

    added: Lesson Domain

---
+++ Src/Core/EduHubAPI.CA.Domain/Lessons/DTOs/LessonDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduHubAPI.CA.Domain.Lessons.DTOs
+{
+    public class LessonDto
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string? VideoUrl { get; set; }
+        public int Order { get; set; }
+        public int? Duration { get; set; }
+        public string? Note { get; set; }
+
+        public long SectionId { get; set; }
+
+        public LessonDto() { }
+    }
+}
---
+++ Src/Core/EduHubAPI.CA.Domain/Lessons/Entities/Lesson.cs
@@ -0,0 +1,30 @@
+﻿using EduHubAPI.CA.Domain.Common;
+using EduHubAPI.CA.Domain.Sections.Entities;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EduHubAPI.CA.Domain.Lessons.Entities
+{
+    public class Lesson : AuditableBaseEntity
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string? VideoUrl { get; set; }
+        public int Order {  get; set; }
+        public int? Duration { get; set; }
+        public string? Note { get; set; }
+
+        public long? SectionId {  get; set; }
+        public Section Section {  get; set; }
+
+        public Lesson(string title)
+        {
+            Title = title;
+        }
+
+        public void Update(string title)
+        {
+            Title = title;
+        }
+    }
+}
