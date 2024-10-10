94f21029edf71966af19749ec29a5a9243aeee5e
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-10-10 23:38:40 +0700

    added: Instructor Domain

---
+++ Src/Core/EduHubAPI.CA.Domain/Instructors/DTOs/InstructorDto.cs
@@ -0,0 +1,20 @@
+﻿using EduHubAPI.CA.Domain.Courses.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduHubAPI.CA.Domain.Instructors.DTOs
+{
+    public class InstructorDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string? Email { get; set; }
+        public string? JobTitle { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? FacebookUrl { get; set; }
+
+        public ICollection<CourseDto> Courses { get; set; }
+    }
+}
---
+++ Src/Core/EduHubAPI.CA.Domain/Instructors/Entities/Instructor.cs
@@ -0,0 +1,29 @@
+﻿using EduHubAPI.CA.Domain.Common;
+using EduHubAPI.CA.Domain.CourseInstructors.Entities;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EduHubAPI.CA.Domain.Instructors.Entities
+{
+    public class Instructor : AuditableBaseEntity
+    {
+        public string Name { get; set; }
+        public string? Email { get; set; }
+        public string? JobTitle { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? FacebookUrl { get; set; }
+
+        public ICollection<CourseInstructor> CourseInstructors { get; set; }
+
+        public Instructor(string name)
+        {
+            Name = name;
+        }
+
+        public void Update(string name)
+        {
+            Name = name;
+        }
+    }
+}
