b3745ea6e65f9a838d9a5a81e7da6a3778a53d31
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-10-16 23:01:48 +0700

    added: Banners Domain

---
+++ Src/Core/EduHubAPI.CA.Domain/Banners/DTOs/BannerDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduHubAPI.CA.Domain.Banners.DTOs
+{
+    public class BannerDto
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string BannerUrl { get; set; }
+        public string? LinkTo { get; set; }
+        public string? Placement { get; set; }
+        public int Priority { get; set; }
+        public bool IsBlank { get; set; }
+        public bool IsPro { get; set; }
+        public DateTime Expired { get; set; }
+        public string? Type { get; set; }
+    }
+}
---
+++ Src/Core/EduHubAPI.CA.Domain/Banners/Entities/Banner.cs
@@ -0,0 +1,26 @@
+﻿using EduHubAPI.CA.Domain.Common;
+using System;
+
+namespace EduHubAPI.CA.Domain.Banners.Entities
+{
+    public class Banner : AuditableBaseEntity
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string BannerUrl { get; set; }
+        public string? LinkTo { get; set; }
+        public string? Placement { get; set; }
+        public int Priority { get; set; }
+        public bool IsBlank { get; set; }
+        public bool IsPro { get; set; }
+        public DateTime Expired { get; set; }
+        public string? Type { get; set; }
+
+
+        public Banner(string name)
+        {
+            BannerUrl = name;
+        }
+    }
+    
+}
