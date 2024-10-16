e23bfab7d727093c3c1e84607a39f60ab54900e9
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-10-16 23:02:26 +0700

    added: CourseInstructors Domain

---
+++ Src/Core/EduHubAPI.CA.Domain/CourseInstructors/Entities/CourseInstructor.cs
@@ -0,0 +1,15 @@
+﻿using EduHubAPI.CA.Domain.Common;
+using EduHubAPI.CA.Domain.Courses.Entities;
+using EduHubAPI.CA.Domain.Instructors.Entities;
+
+namespace EduHubAPI.CA.Domain.CourseInstructors.Entities
+{
+    public class CourseInstructor : AuditableBaseEntity
+    {
+        public long? CourseId { get; set; }
+        public Course Course { get; set; }
+
+        public long? InstructorId { get; set; }
+        public Instructor Instructor { get; set; } 
+    }
+}
